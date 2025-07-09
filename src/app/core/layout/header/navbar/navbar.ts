566faed9c654544bf74d78f39f20dc68f28b7bf9
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-09 20:56:25 +0200

    feat(navbar): implement dynamic navigation with new navlink structure
    - refactor navbar component to use NAVIGATION_ITEMS for menu items
    - add dark mode button and mobile navigation component
    - update styles for animated underline effect on hover

--- src/app/core/layout/header/navbar/navbar.ts
+++ src/app/core/layout/header/navbar/navbar.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ScrollService } from '@core/services/scroll.service';
 import { ButtonDarkMode } from '../button-dark-mode/button-dark-mode';
+import { NAVIGATION_ITEMS } from '../constants/navlink';
+import { NavMobile } from '../nav-mobile/nav-mobile';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonDarkMode],
+  imports: [ButtonDarkMode, NavMobile],
   templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  navigationItems = NAVIGATION_ITEMS;
+  private readonly scrollService = inject(ScrollService);
+
+  onNavigationClick(fragment?: string): void {
+    this.scrollService.scrollToSection(fragment);
+  }
+}
