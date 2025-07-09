22fc1c06063997828c786832daa154d448a8446a
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-09 21:06:39 +0200

    feat(home-laptop): ajouter le composant home-laptop avec template et styles associés
    ajout des fichiers HTML, CSS et de test pour le composant home-laptop.

---
+++ src/app/features/visitor/home/home-laptop/home-laptop.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-laptop',
+  imports: [],
+  templateUrl: './home-laptop.html',
+  styleUrl: './home-laptop.css',
+})
+export class HomeLaptop {}
