d3320c43982df03f33f73ed16d76e312330e595e
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-09 21:12:30 +0200

    feat(app): améliorer la config d'application avec la prise en charge de la localisation française
    - ajout de la prise en charge de la langue française dans la configuration de l'application
    - réorganisation de la structure HTML pour une meilleure lisibilité
    - ajout de composants manquants dans le module principal

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,22 @@
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+
+registerLocaleData(localeFr);
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: LOCALE_ID, useValue: 'fr' }
+      ]
     }).compileComponents();
   });
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { Navbar } from './core/layout/header/navbar/navbar';
+import { Navbar } from '@core/layout/header/navbar/navbar';
+import { About } from '@features/visitor/about/about';
+import { Contact } from '@features/visitor/contact/contact';
+import { Home } from '@features/visitor/home/home';
+import { Project } from '@features/visitor/project/project';
+import { Skills } from '@features/visitor/skills/skills';
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, Home, About, Skills, Project, Contact, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
