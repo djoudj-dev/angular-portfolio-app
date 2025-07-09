2f3e366f5b3e6a8a90986f2c57ffec98585c6d4a
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-09 08:38:56 +0200

    feat(supabase): ajout de la configuration Supabase et des services associés
    - ajout d'un service Supabase pour la gestion des utilisateurs et des profils
    - configuration des environnements de développement avec les clés Supabase
    - mise à jour de la configuration TypeScript pour les chemins d'importation
    - ajout de remplacements de fichiers pour l'environnement de développement

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wncduunijnthqxogmtjh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduY2R1dW5pam50aHF4b2dtdGpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwMzc0MzgsImV4cCI6MjA2NzYxMzQzOH0.V9H95kQnMkZZO10F0kriYTMQL4F-vYv7Y_XoeWELrjE',
+}
