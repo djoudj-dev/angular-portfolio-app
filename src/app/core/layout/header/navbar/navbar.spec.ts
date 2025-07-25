a05802036036add3fc3eb18e729f4d79fe4a6061
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-25 14:52:26 +0200

    test(router): add `RouterTestingModule` and `provideRouter` to app and navbar specs
    - Include `RouterTestingModule` in test imports for `App` and `Navbar` to enable routing tests.
    - Provide empty routes with `provideRouter` for isolated unit test environments.

--- src/app/core/layout/header/navbar/navbar.spec.ts
+++ src/app/core/layout/header/navbar/navbar.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Navbar } from './navbar';
 
@@ -8,7 +10,10 @@ describe('Navbar', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Navbar],
+      imports: [Navbar, RouterTestingModule],
+      providers: [
+        provideRouter([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Navbar);
